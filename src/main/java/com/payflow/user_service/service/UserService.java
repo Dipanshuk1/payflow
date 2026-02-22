39ff2230b2e03d527ba3fc68c7fbad54e2807f59
Dipanshuk1 <dipanshukumar1936@gmail.com>
2026-02-23 01:22:04 +0530

    feat(user-service): add user service interface with basic operations

--- src/main/java/com/payflow/user_service/service/UserService.java
+++ src/main/java/com/payflow/user_service/service/UserService.java
@@ -1,4 +1,15 @@
-package com.payflow.user_service.service;
+package com.paypal.user_service.service;
+
+import com.payflow.user_service.entity.User;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface UserService {
+
+    User createUser(User user);
+
+    Optional<User> getUserById(Long id);
+
+    List<User> getAllUsers();
 }
